2ca8548b18182738ee45267ad2d9f0ca4a59b3e7
neevlokchandani <73300169+neevlokchandani@users.noreply.github.com>
2021-01-15 18:35:44 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PRO-C40
+# C39-Debug-Part-1
+this fruitcatcher 2 it is a two player game the link is given below ==>
+https://pranay1234567890.github.io/Fruitcatcher-2/
---
+++ game.js
@@ -0,0 +1,112 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player_img);
+    
+    player2 = createSprite(800,500);
+    player2.addImage("player2", player_img);
+    players=[player1,player2];
+
+        }
+    
+    play(){
+        
+                form.hide();
+
+                Player.getPlayerInfo();
+                 image(back_img, 0, 0, 1000, 800);
+                 var x =100;
+                 var y=200;
+                
+                 drawSprites();
+                 for(var plr in allPlayers){
+                    var index =0;
+                     index = index+1;
+                     x = 500-allPlayers[plr].distance;
+                     y=500;
+
+                    
+                     
+                     players[index -1].x =mouseX;
+                     players[index - 1].y = y;
+                       players[index].visible=false;
+  
+                    
+                         textSize(25);
+                         fill("white");
+                         text(allPlayers.player1.name + "'s Score:"+allPlayers.player1.score,50,50);
+                        text(allPlayers.player2.name + "'s Score:" + allPlayers.player2.score, 50, 100);
+                 
+                 }
+                
+                
+                 
+
+                 
+            
+                 if (frameCount % 20 === 0) {
+                     fruits = createSprite(random(100, 1000), 0, 100, 100);
+                     fruits.velocityY = 6;
+                     var rand = Math.round(random(1,5));
+                     switch(rand){
+                         case 1: fruits.addImage("fruit1",fruit1_img);
+                         break;
+                         case 2: fruits.addImage("fruit1", fruit2_img);
+                         break;
+                         case 3: fruits.addImage("fruit1", fruit3_img);
+                         break;
+                         case 4: fruits.addImage("fruit1", fruit4_img);
+                         break;
+                         case 5: fruits.addImage("fruit1", fruit5_img);
+                         break;
+                     }
+                     fruitGroup.add(fruits);
+                     
+                 }
+                 
+                  if (player.index !== null) {
+                      for(var i = 0; i< fruitGroup.length;i++){
+                          if(fruitGroup.get(i).isTouching(players)){
+                            fruitGroup.get(i).destroy();
+                            player.score =player.score+1;
+                            player.update();
+                          }
+                       
+                      }
+                            
+                        
+                  }
+               
+    }
+//carsAtEnd
+    end(){
+       console.log("Game Ended");
+
+    }
+}
